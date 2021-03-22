a71707948fdde80a33f1b423e3125a5d23fb3806
Michael Eclavea <usermike@gmail.com>
2021-03-22 01:15:09 -0700

    added fluent ui button

---
+++ src/pages/FluentUi.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FluentButton from '../components/fluent-ui/FluentButton'
+
+const FluentUiPage = () => {
+    return(
+        <>
+        <h1>Fluent UI Page</h1>
+        <FluentButton/>
+        </>
+    )
+}
+
+export default FluentUiPage
